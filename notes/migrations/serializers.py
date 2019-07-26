2843d3e4a657eb9a3144912dfb220be639f35f5b
madalina-ceban <madalina.ceban55@gmail.com>
2019-07-26 10:33:52 +0300

    Created new serializer for Note model

---
+++ notes/migrations/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework.serializers import ModelSerializer
+
+
+class NoteSerializer(ModelSerializer): 
+    class Meta: 
+        model = Note
+        fields = '__all__'
+        
+
+
