899bd45309cbcb610466e6b008d5e31c647d8cad
alexandrina2510 <valeriapichin@gmail.com>
2019-07-26 10:37:49 +0300

    "definirea modelului note in models.py"

--- notes/models.py
+++ notes/models.py
@@ -3,3 +3,7 @@ from django.contrib.auth.models import AbstractUser
 
 class GGITUser(AbstractUser):
     settings = models.TextField()
+
+class Note (models.Model)   
+    user = FK(User)
+    created_at =DateField
