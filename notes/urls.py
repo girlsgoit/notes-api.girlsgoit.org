f021af1d2b7d4afc524468011d09c8afef5d03d1
cristinatimbur24 <ctimbur@mail.ru>
2019-07-26 11:48:00 +0300

    define a new notes URL

--- notes/urls.py
+++ notes/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import index
+from .views import index, note_list
 
 urlpatterns = [
     path('', index, name='index'),
+    path('notes/', note_list, name = 'note_list'),
 ]
--- notes/views.py
+++ notes/views.py
@@ -1,4 +1,16 @@
 from django.http import HttpResponse
+from .models import Note
+from .serializers import NoteSerializer
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
 def index(request):
     return HttpResponse('Hello Git')
+
+@api_view(['GET'])
+def note_list(request):
+    if request.method == 'GET':
+        list_of_notes = Note.objects.all()
+        serializer_notes = NoteSerializer(list_of_notes, many=True)
+        return Response(serializer_notes.data)
+
