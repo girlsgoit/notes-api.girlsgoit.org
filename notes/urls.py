5a17f9da54c28a7269d77f9f026473d6acb88f44
alexandrina2510 <valeriapichin@gmail.com>
2019-07-26 12:54:13 +0300

    "Am definit URL-ul note-elements"

--- notes/urls.py
+++ notes/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import index, note_list
+from .views import index, note_list, note_elements_list
 
 urlpatterns = [
     path('', index, name='index'),
     path('notes/', note_list, name = 'note_list'),
+    path('note-elements', note_elements_list, name ="note_elements_list")
 ]
--- notes/views.py
+++ notes/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
-from .models import Note
-from .serializers import NoteSerializer
+from .models import Note, NoteElement
+from .serializers import NoteSerializer, NoteElementSerializer
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
@@ -14,3 +14,9 @@ def note_list(request):
         serializer_notes = NoteSerializer(list_of_notes, many=True)
         return Response(serializer_notes.data)
 
+@api_view(['GET'])
+def note_elements_list(request):
+     if request.method == 'GET':
+        list_of_elements = NoteElement.objects.all()
+        serializer_elements = NoteElementSerializer(list_of_elements, many=True)
+        return Response(serializer_elements.data)   
