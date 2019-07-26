068e69dc66fcd925a32a041f0857667dad97d38c
madalina-ceban <madalina.ceban55@gmail.com>
2019-07-26 11:49:05 +0300

    created NoteElementSerializer

--- notes/serializers.py
+++ notes/serializers.py
@@ -1,8 +1,18 @@
 from rest_framework.serializers import ModelSerializer
-from .models import Note
+from .models import Note, NoteElement
 
 class NoteSerializer(ModelSerializer): 
     class Meta: 
         model = Note
         fields = '__all__'
 
+
+
+
+
+
+
+class NoteElementSerializer(ModelSerializer):
+    class Meta:
+        model = NoteElement
+        fields = '__all__'
