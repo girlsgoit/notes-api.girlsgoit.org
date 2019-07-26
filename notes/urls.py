bbc23e163108e01b338c06f9a316bd47f5e808c2
cristinatimbur24 <ctimbur@mail.ru>
2019-07-26 13:39:17 +0300

    define user_details

--- notes/urls.py
+++ notes/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import index, note_list, users, note_elements_list, note_details, notes
+from .views import index, note_list, users, note_elements_list, note_details,user_details
 
 urlpatterns = [
     path('', index, name='index'),
@@ -8,4 +8,5 @@ urlpatterns = [
     path('notes/<int:note_id>', note_details, name = "note_details"),
     path('note-elements', note_elements_list, name ="note_elements_list"),
     path('users/', users, name = 'users'),
+    path('users/<int:user_id>', user_details, name= 'user_details'),
 ]
