c6650c0a5b16b07bff687f229e67cceafde2fcef
Marc Graciov <mark.graciov@gmail.com>
2019-07-27 00:11:04 +0300

    update base

--- notes_api/settings/base.py
+++ notes_api/settings/base.py
@@ -18,12 +18,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'vdzb!z99heg#(b8p)u%&rpy_fsoe)@vy-%#hrdcjjki69+2rk8'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
 ALLOWED_HOSTS = ['localhost', '.girlsgoit.org', '.azurewebsites.net']
 
 # Application definition
