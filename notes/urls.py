44ad5bce445a02ef0f7475022b032f50be863e1b
Renata21 <celac.renata@gmail.com>
2019-07-26 12:33:11 +0300

    am definit "note_details"

--- notes/urls.py
+++ notes/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from .views import index
+from .views import index, note_details
 
 urlpatterns = [
     path('', index, name='index'),
+
+    path('notes/<int:note_id>', note_details, name = "note_details"),
 ]
--- notes/views.py
+++ notes/views.py
@@ -1,4 +1,26 @@
 from django.http import HttpResponse
-
+from rest_framework.response import Response
+from .serializers import NoteSerializer
+from .models import Note
+from rest_framework.decorators import api_view
+from django.shortcuts import get_object_or_404, render
 def index(request):
     return HttpResponse('Hello Git')
+
+
+
+
+
+
+
+
+
+
+
+@api_view(["GET"])
+def note_details(request, note_id):
+    note = get_object_or_404(Note, pk = note_id)
+
+    if request.method == "GET":
+        serialized_note = NoteSerializer(note)
+        return Response(serialized_note.data)
